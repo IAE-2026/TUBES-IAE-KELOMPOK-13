e9c745e9928a952e5982e0335265336dd30ec0bf
zidan aliansyah shidiq <zidanaliansyah11@gmail.com>
2026-06-19 14:43:47 +0700

    fix: update route dari doctor-schedules ke schedules sesuai kontrak

--- jadwaldokter-service/routes/api.php
+++ jadwaldokter-service/routes/api.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\DoctorScheduleController;
 
 Route::prefix('v1')->group(function () {
-    Route::get('/doctor-schedules', [DoctorScheduleController::class, 'index']);
-    Route::get('/doctor-schedules/{id}', [DoctorScheduleController::class, 'show']);
-    Route::post('/doctor-schedules', [DoctorScheduleController::class, 'store']);
+    Route::get('/schedules', [DoctorScheduleController::class, 'index']);
+    Route::get('/schedules/{id}', [DoctorScheduleController::class, 'show']);
+    Route::post('/schedules', [DoctorScheduleController::class, 'store']);
 });
