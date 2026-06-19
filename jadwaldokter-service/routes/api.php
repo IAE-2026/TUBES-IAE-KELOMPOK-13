1e92e889bbbb9ad9332ee4b28d0c624d056e4623
widiamsr <widiamesra1301@gmail.com>
2026-06-19 18:37:30 +0700

    feat: setup Docker, API Gateway Nginx, dan fix routing semua service

--- jadwaldokter-service/routes/api.php
+++ jadwaldokter-service/routes/api.php
@@ -1,10 +1,9 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\DoctorScheduleController;
 
 Route::prefix('v1')->group(function () {
-    Route::get('/doctor-schedules', [DoctorScheduleController::class, 'index']);
-    Route::get('/doctor-schedules/{id}', [DoctorScheduleController::class, 'show']);
-    Route::post('/doctor-schedules', [DoctorScheduleController::class, 'store']);
+    Route::get('/schedules', [DoctorScheduleController::class, 'index']);
+    Route::get('/schedules/{id}', [DoctorScheduleController::class, 'show']);
+    Route::post('/schedules', [DoctorScheduleController::class, 'store']);
 });
